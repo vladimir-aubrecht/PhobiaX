7480b9824b93df8b89867c12ef9f2eac3cdaf3c9
Vladimír Aubrecht <vladimiraubrecht@Vladimirs-MacBook-Pro.local>
2020-04-08 02:14:34 +0200

    Introduced animated asset.

---
+++ PhobiaX/Assets/AnimatedAsset.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using PhobiaX.SDL2;
+
+namespace PhobiaX.Assets
+{
+    public class AnimatedAsset : IDisposable
+    {
+        private IList<SDLSurface> surfaces = new List<SDLSurface>();
+        private int currentFrame = 0;
+
+        public AnimatedAsset(IList<SDLSurface> surfaces)
+        {
+            this.surfaces = surfaces;
+        }
+
+        public void PreviousFrame()
+        {
+            currentFrame--;
+
+            if (currentFrame < 0)
+            {
+                currentFrame = this.surfaces.Count - 1;
+            }
+        }
+
+        public void NextFrame()
+        {
+            currentFrame++;
+
+            if (currentFrame == this.surfaces.Count)
+            {
+                currentFrame = 0;
+            }
+        }
+
+        public void ResetFrame()
+        {
+            currentFrame = 0;
+        }
+
+        public int GetCurrentFrameIndex()
+        {
+            return currentFrame;
+        }
+
+        public SDLSurface GetCurrentFrame()
+        {
+            return surfaces[currentFrame];
+        }
+
+        public IList<SDLSurface> GetAnimationFrames()
+        {
+            return surfaces;
+        }
+
+        public void Dispose()
+        {
+            foreach (var surface in surfaces)
+            {
+                surface.Dispose();
+            }
+        }
+    }
+}
--- PhobiaX/Assets/AnimatedSurfaceAssets.cs
+++ PhobiaX/Assets/AnimatedSurfaceAssets.cs
@@ -6,65 +6,49 @@ namespace PhobiaX.Assets
 {
     public class AnimatedSurfaceAssets : IDisposable
     {
-        private Dictionary<string, IList<SDLSurface>> animations = new Dictionary<string, IList<SDLSurface>>();
-        private Dictionary<string, int> currentFrames = new Dictionary<string, int>();
+        private Dictionary<string, AnimatedAsset> animations = new Dictionary<string, AnimatedAsset>();
 
         public void AddAnimation(string name, IList<SDLSurface> animation)
         {
-            animations.Add(name, animation);
-            currentFrames.Add(name, 0);
+            animations.Add(name, new AnimatedAsset(animation));
         }
 
         public void Dispose()
         {
             foreach (var animation in animations)
             {
-                foreach (var surface in animation.Value)
-                {
-                    surface.Dispose();
-                }
+                animation.Value.Dispose();
             }
         }
 
         public void PreviousFrame(string name)
         {
-            currentFrames[name]--;
-
-            if (currentFrames[name] < 0)
-            {
-                currentFrames[name] = animations[name].Count - 1;
-            }
+            animations[name].PreviousFrame();
         }
 
         public void NextFrame(string name)
         {
-            currentFrames[name]++;
-
-            if (currentFrames[name] == animations[name].Count)
-            {
-                currentFrames[name] = 0;
-            }
+            animations[name].NextFrame();
         }
 
         public void ResetFrame(string name)
         {
-            currentFrames[name] = 0;
+            animations[name].ResetFrame();
         }
 
         public int GetCurrentFrameIndex(string name)
         {
-            return currentFrames[name];
+            return animations[name].GetCurrentFrameIndex();
         }
 
         public SDLSurface GetCurrentFrame(string name)
         {
-            var index = currentFrames[name];
-            return animations[name][index];
+            return animations[name].GetCurrentFrame();
         }
 
         public IList<SDLSurface> GetAnimationFrames(string name)
         {
-            return animations[name];
+            return animations[name].GetAnimationFrames();
         }
     }
 }
